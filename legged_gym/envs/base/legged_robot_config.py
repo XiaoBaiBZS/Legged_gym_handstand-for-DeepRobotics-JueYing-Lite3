ca6a88a337d1f1088a8f2ed78f923a32359ffce6
ZhanshuoBai <1298589907@qq.com>
2025-11-01 07:16:32 +0800

    修改了一些参数，，训练 19000 轮之后的 mujoco 效果还可以

--- legged_gym/envs/base/legged_robot_config.py
+++ legged_gym/envs/base/legged_robot_config.py
@@ -130,10 +130,10 @@ class LeggedRobotCfg(BaseConfig):
     class rewards:
         class scales:
             termination = -0.0
-            tracking_lin_vel = 3. #3.0
-            tracking_ang_vel = 0.2 #1.5
+            tracking_lin_vel = -0.2 #3.0
+            tracking_ang_vel = -0.2 #1.5
             lin_vel_z = -0.
-            ang_vel_xy = -0.2
+            ang_vel_xy = -0.3
             orientation = -0.0
             torques = -0.00001
             dof_vel = -0.
@@ -143,14 +143,14 @@ class LeggedRobotCfg(BaseConfig):
             collision = -1.
             feet_stumble = -0.0 
             action_rate = -0.02
-            stand_still = -0.35
+            stand_still = -0.3 #0
             handstand_feet_height_exp = 10.0 #10
-            handstand_feet_on_air = 2 #1.0
-            handstand_feet_air_time = 2 #1.0
+            handstand_feet_on_air = 2.5 #1.0
+            handstand_feet_air_time = 2.5 #1.0
             handstand_orientation_l2 = -0.5
             
         only_positive_rewards = False # if true negative total rewards are clipped at zero (avoids early termination problems)
-        tracking_sigma = 0.25 # tracking reward = exp(-error^2/sigma)
+        tracking_sigma = 0.2 # tracking reward = exp(-error^2/sigma) 0.25
         soft_dof_pos_limit = 1. # percentage of urdf limits, values above this limit are penalized
         soft_dof_vel_limit = 1.
         soft_torque_limit = 1.
@@ -250,7 +250,7 @@ class LeggedRobotCfgPPO(BaseConfig):
         policy_class_name = 'ActorCritic'
         algorithm_class_name = 'PPO'
         num_steps_per_env = 24 # per iteration
-        max_iterations = 500 # number of policy updates
+        max_iterations = 50 # number of policy updates
 
         # logging
         save_interval = 50 # check for potential saves every this many iterations
